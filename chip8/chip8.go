3929261c579114d05c4003f1ca1253db56e88496
James O'Toole <jimmeyotoole@gmail.com>
2016-06-02 18:37:12 +1000

    Test for 1NNN

--- chip8/chip8.go
+++ chip8/chip8.go
@@ -15,14 +15,14 @@ type Chip8 struct {
 	*/
 
 	//Ram for the whole system. 4x1024 byes available
-	memory [4096]byte
+	Memory [4096]byte
 	// V is for the CPU Registers. v0,v1... v15. Last one is a carry flag
 	V [16]byte
 
 	//Index register + program counter,
 
-	pc     uint16 //Program Counter
-	opcode uint16 //Current Op Code
+	Pc     uint16 //Program Counter
+	Opcode uint16 //Current Op Code
 	index  uint16 //Index Register
 	sp     uint16 //Stack Position
 
@@ -40,12 +40,12 @@ type Chip8 struct {
 	key [16]byte
 }
 
-// Initialize registers and memory once
+// Initialize registers and Memory once
 func (self *Chip8) Init() {
 	fmt.Printf("Chip 8 Initalising...\n")
 
-	self.pc = 0x200 // Program counter starts at 0x200, the space of memory after the interpreter
-	self.opcode = 0 // Reset current opcode
+	self.Pc = 0x200 // Program counter starts at 0x200, the space of Memory after the interpreter
+	self.Opcode = 0 // Reset current Opcode
 	self.index = 0  // Reset index register
 	self.sp = 0     // Reset stack pointer
 
@@ -53,7 +53,7 @@ func (self *Chip8) Init() {
 
 }
 
-//Read file in curent dir into memory
+//Read file in curent dir into Memory
 func (self *Chip8) LoadGame(filename string) {
 	rom, _ := ioutil.ReadFile(filename)
 	rom_length := len(rom)
@@ -64,58 +64,58 @@ func (self *Chip8) LoadGame(filename string) {
 	//If room to store ROM in RAM
 	if (4096 - 512) > rom_length {
 		for i := 0; i < rom_length; i++ {
-			self.memory[i+512] = rom[i]
+			self.Memory[i+512] = rom[i]
 		}
 	}
 
-	fmt.Printf("Rom %s loaded into memory\n", filename)
+	fmt.Printf("Rom %s loaded into Memory\n", filename)
 }
 
 //Tick to load next emulation cycle
 func (self *Chip8) EmulateCycle() {
 	// Fetch Opcode
-	b1 := uint16(self.memory[self.pc])
-	b2 := uint16(self.memory[self.pc+1])
+	b1 := uint16(self.Memory[self.Pc])
+	b2 := uint16(self.Memory[self.Pc+1])
 
 	//Bitwise, add padding to end of first byte and append second byte to end
-	self.opcode = (b1 << 8) | b2
+	self.Opcode = (b1 << 8) | b2
 
-	fmt.Printf("OpCode = %#02x\n", self.opcode)
+	fmt.Printf("OpCode = %#02x\n", self.Opcode)
 	// Decode Opcode
-	switch self.opcode & 0xF000 {
+	switch self.Opcode & 0xF000 {
 	case 0xA000: // ANNN: Sets I to the address NNN
-		// Execute opcode
-		self.index = self.opcode & 0x0FFF
-		self.pc += 2
+		// Execute Opcode
+		self.index = self.Opcode & 0x0FFF
+		self.Pc += 2
 		break
 
 	//1 to 7, jump, call and skip instructions
 	case 0x1000: // 0x1NNN: Jumps to address NNN
-		self.pc = self.opcode & 0x0FFF
+		self.Pc = self.Opcode & 0x0FFF
 		break
 	case 0x2000: // 0x2NNN: Calls subroutine at NNN.
-		self.stack[self.sp] = self.pc  // Store current address in stack
+		self.stack[self.sp] = self.Pc  // Store current address in stack
 		self.sp++                      // Increment stack pointer
-		self.pc = self.opcode & 0x0FFF // Set the program counter to the address at NNN
+		self.Pc = self.Opcode & 0x0FFF // Set the program counter to the address at NNN
 		break
 	case 0x3000: // 0x3XNN: Skips the next instruction if VX equals NN
-		if uint16(self.V[(self.opcode&0x0F00)>>8]) == self.opcode&0x00FF {
-			self.pc += 4
+		if uint16(self.V[(self.Opcode&0x0F00)>>8]) == self.Opcode&0x00FF {
+			self.Pc += 4
 		} else {
-			self.pc += 2
+			self.Pc += 2
 		}
 		break
 	case 0x4000: // 0x4XNN: Skips the next instruction if VX doesn't equal NN.
-		if uint16(self.V[(self.opcode&0x0F00)>>8]) != self.opcode&0x00FF {
-			self.pc += 4
+		if uint16(self.V[(self.Opcode&0x0F00)>>8]) != self.Opcode&0x00FF {
+			self.Pc += 4
 		} else {
-			self.pc += 2
+			self.Pc += 2
 		}
 	case 0x5000: // 0x5XY0: Skips the next instruction if VX equals VY.
-		if uint16(self.V[(self.opcode&0x0F00)>>8]) != uint16(self.V[self.opcode&0x0F0]>>4) {
-			self.pc += 4
+		if uint16(self.V[(self.Opcode&0x0F00)>>8]) != uint16(self.V[self.Opcode&0x0F0]>>4) {
+			self.Pc += 4
 		} else {
-			self.pc += 2
+			self.Pc += 2
 		}
 
 	default:
---
+++ chip8/chip8_test.go
@@ -0,0 +1,43 @@
+package chip8_test
+
+import (
+	"fmt"
+	"github.com/bomer/chip8/chip8"
+	"testing"
+)
+
+var myChip8 chip8.Chip8
+
+func Prep() {
+	myChip8.Init()
+	myChip8.LoadGame("../pong.c8")
+}
+
+func TestInit(t *testing.T) {
+	Prep()
+	// fmt.Println("mem = %s", myChip8.Memory)
+	fmt.Println("Starting pc  = %s", myChip8.Pc)
+	if myChip8.Pc != 512 {
+		t.Error("Error -  Did not initalise correctly")
+	}
+}
+
+func TestOpCode1NNN(t *testing.T) {
+
+	Prep()
+	//Set instructions to, 1, Move to, and 0x0226, pos 552
+	myChip8.Memory[512] = 0x12
+	myChip8.Memory[513] = 0x26
+	myChip8.EmulateCycle()
+
+	fmt.Println("PC=== %s", myChip8.Pc)
+	if myChip8.Pc != 550 {
+		t.Error("Did not start in the correct Program counter")
+	}
+	myChip8.EmulateCycle()
+
+	if myChip8.Pc != 552 {
+		t.Error("Did not move the Program counter correctly")
+	}
+
+}
