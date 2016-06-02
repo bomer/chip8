26b2ea23c75812dc65d713fed0ed1e54a4a930bd
James O'Toole <jimmeyotoole@gmail.com>
2016-06-03 09:54:46 +1000

    Unit Test for Op Code 3XNN

--- chip8/chip8_test.go
+++ chip8/chip8_test.go
@@ -58,5 +58,30 @@ func TestOpCode2NNN(t *testing.T) {
 	if myChip8.Pc != 544 {
 		t.Error("Did not move the Program counter correctly")
 	}
+}
 
+func TestOpCode3XNN(t *testing.T) {
+	Prep()
+	if myChip8.Sp != 0 {
+		t.Error("Did not start in the correct Stack Position")
+	}
+
+	// Fail Case, does not match so only progress 2, stores 0 514
+	myChip8.V[0] = 0x02
+	//Set instructions to, 1, Move to, and 0x0226, pos 552
+	myChip8.Memory[512] = 0x30
+	myChip8.Memory[513] = 0x0f
+	myChip8.EmulateCycle()
+
+	if myChip8.Pc != 0x202 { //514
+		t.Error("Did not Update the Stack Position correctly")
+	}
+
+	myChip8.Pc = 512
+	myChip8.Memory[512] = 0x30
+	myChip8.Memory[513] = 0x02
+	myChip8.EmulateCycle()
+	if myChip8.Pc != 0x0204 { //516
+		t.Error("Did not Update the Stack Position correctly")
+	}
 }
